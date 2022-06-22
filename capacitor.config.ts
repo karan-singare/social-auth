c8006431f0986050d7b2c7e4231ee67991eac66d
Karan Singare <karansingare@gmail.com>
2022-06-23 02:21:14 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'social-auth',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
