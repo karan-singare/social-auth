a1609fe057ee8e5a78cbf7d3139d5324127e3e0a
Karan Singare <karansingare@gmail.com>
2022-06-29 17:12:52 +0530

    social-auth:- temp-changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,36 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'tv.mogiio.ott.dev.arka',
   appName: 'social-auth',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  cordova: {},
+  plugins: {
+    GoogleAuth: {
+      "scopes": ["profile", "email"],
+      'serverClientId': '541915483304-542ghfs1h8dcqs1qmma9qffriqk9qhep.apps.googleusercontent.com',
+      'forceCodeForRefreshToken': true
+    },
+    SplashScreen: {
+      "launchShowDuration": 1000,
+      "launchAutoHide": false,
+      "backgroundColor": "#000000",
+      "androidSplashResourceName": "splash",
+      "androidScaleType": "CENTER_CROP",
+      "showSpinner": true,
+      "androidSpinnerStyle": "horizontal",
+      "iosSpinnerStyle": "small",
+      "spinnerColor": "#999999",
+      "splashFullScreen": true,
+      "splashImmersive": false,
+      "layoutName": "launch_screen",
+      "useDialog": false
+    },
+    PushNotifications: {
+      presentationOptions: ["badge", "sound", "alert"],
+    },
+  }
 };
 
 export default config;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {GoogleAuth} from "@codetrix-studio/capacitor-google-auth";
+import {Plugins} from "@capacitor/core";
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
-  ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+export class AppComponent implements OnInit {
+  userInfo: any = null;
   constructor() {}
+
+  ngOnInit(): void {
+    GoogleAuth.initialize({
+      clientId: '541915483304-542ghfs1h8dcqs1qmma9qffriqk9qhep.apps.googleusercontent.com',
+      scopes: ['profile', 'email'],
+      grantOfflineAccess: true,
+    });
+  }
+
+  async googleSignIn() {
+    try {
+      const googleUser = await Plugins.GoogleAuth.signIn(null);
+      this.userInfo = googleUser;
+      console.log(googleUser);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
